f30adc3c7e9651e148b7f83e1fe6d190eb9306a7
113xiaoji <31875988+113xiaoji@users.noreply.github.com>
2026-03-15 18:22:15 +0800

    jit: gate enum flag and after upstream merge

--- cinderx/Jit/hir/builder.cpp
+++ cinderx/Jit/hir/builder.cpp
@@ -5987,6 +5987,11 @@ BorrowedRef<> HIRBuilder::constArg(const BytecodeInstruction& bc_instr) {
 }
 
 void HIRBuilder::checkTranslate() {
+  if (preloader_.fullname() == "enum:Flag.__and__") {
+    throw std::runtime_error{
+        "Cannot compile enum:Flag.__and__ to HIR after upstream merge"};
+  }
+
   PyObject* names = code_->co_names;
   std::unordered_set<Py_ssize_t> banned_name_ids;
   auto name_at = [&](Py_ssize_t i) {
